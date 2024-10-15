6382cb3217642f2443d327ce394051bae4aa1464
Mrglxor <muhamadfarhan.inc@gmail.com>
2024-10-15 19:08:05 +0700

    adsense implementation

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,6 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta name="google-adsense-account" content="ca-pub-7686429509274648" />
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7686429509274648"
