7659769a6ff877391e5b3395cce256323726ee02
Mrglxor <muhamadfarhan.inc@gmail.com>
2024-11-17 19:07:50 +0700

    fix api cron vercel

--- src/app/api/cron/route.js
+++ src/app/api/cron/route.js
@@ -2,11 +2,11 @@ import { NextResponse } from "next/server";
 import saveAllCategoriesToDB from "../../../../lib/newsAPI";
 
 export async function GET(req) {
-  const authHeader = req.headers.get("Authorization");
+  // const authHeader = req.headers.get("Authorization");
 
-  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-  }
+  // if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+  //   return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  // }
 
   console.log("Fetching news and saving to DB...");
   await saveAllCategoriesToDB();
